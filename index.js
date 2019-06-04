21b344fee1f91f2ee23d570243ab3d695d1bac49
abab0528 <abab0528@naver.com>
2019-06-04 15:14:09 +0900

    middlewares

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-const express = require('express')
-const app = express()
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+const app = express();
 
 const PORT = 4000;
 
-function handleListening(){
-    console.log(`Listening on: http://localhost:${PORT}`);
-}
+const handleListening = () => 
+console.log(`Listening on: http://localhost:${PORT}`);
 
-function handleHome(req,res){
-    console.log(req);
-    res.send("Hello from home");
-}
 
-function handleProfile(req,res){
-    res.send("You are on my profile");
-}
+const handleHome = (req,res) =>  res.send("Hello from my ass");
+
+const handleProfile = (req,res) => res.send("You are on my profile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(helmet());
+app.use(morgan("dev"));
+
 app.get("/",handleHome);
 app.get("/profile",handleProfile);
+
 app.listen(PORT, handleListening);
